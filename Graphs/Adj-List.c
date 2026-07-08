a339d4a5b9c79feb37c7f4a270ad8fd19bf70e85
5|-|4|-|1d <shahidsaleemitoo@gmail.com>
2026-07-08 12:17:08 +0530

    Add adjacency list representation in Adj-List.c

---
+++ Graphs/Adj-List.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Adjacency List Representation\n\n");
+
+    printf("0 -> 1 -> 2\n");
+    printf("1 -> 0 -> 2\n");
+    printf("2 -> 0 -> 1\n");
+
+    return 0;
+}
