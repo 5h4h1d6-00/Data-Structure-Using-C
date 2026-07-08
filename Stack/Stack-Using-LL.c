778479dced379b84245f956244d6b0290dcf90a7
5|-|4|-|1d <shahidsaleemitoo@gmail.com>
2026-07-08 12:12:48 +0530

    Implement stack using linked list in C

---
+++ Stack/Stack-Using-LL.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *top = NULL;
+
+void push(int value)
+{
+    struct Node *newNode;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = top;
+
+    top = newNode;
+}
+
+void pop()
+{
+    if(top == NULL)
+    {
+        printf("Stack Underflow\n");
+        return;
+    }
+
+    struct Node *temp = top;
+
+    printf("Deleted Element = %d\n", top->data);
+
+    top = top->next;
+
+    free(temp);
+}
+
+void display()
+{
+    struct Node *temp = top;
+
+    printf("Stack Elements:\n");
+
+    while(temp != NULL)
+    {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    pop();
+
+    display();
+
+    return 0;
+}
