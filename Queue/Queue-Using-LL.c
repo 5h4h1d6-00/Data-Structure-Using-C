7f72dd110d32badd525dbede2dfebd1e19825be8
5|-|4|-|1d <shahidsaleemitoo@gmail.com>
2026-07-08 12:13:30 +0530

    Create Queue-Using-LL.c

---
+++ Queue/Queue-Using-LL.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *front = NULL;
+struct Node *rear = NULL;
+
+void enqueue(int value)
+{
+    struct Node *newNode;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if(rear == NULL)
+    {
+        front = rear = newNode;
+        return;
+    }
+
+    rear->next = newNode;
+    rear = newNode;
+}
+
+void dequeue()
+{
+    if(front == NULL)
+    {
+        printf("Queue Underflow\n");
+        return;
+    }
+
+    struct Node *temp = front;
+
+    printf("Deleted Element = %d\n", front->data);
+
+    front = front->next;
+
+    if(front == NULL)
+        rear = NULL;
+
+    free(temp);
+}
+
+void display()
+{
+    struct Node *temp = front;
+
+    printf("Queue Elements:\n");
+
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+
+    display();
+
+    return 0;
+}
