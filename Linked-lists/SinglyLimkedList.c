a89934e04621e4501e04921680339a833ac9cc61
5|-|4|-|1d <shahidsaleemitoo@gmail.com>
2026-07-08 12:06:36 +0530

    Add basic singly linked list implementation
    Implement a simple singly linked list with two nodes and print the list.

---
+++ Linked-lists/SinglyLimkedList.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *first = NULL;
+    struct Node *second = NULL;
+
+    first = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+
+    first->data = 10;
+    first->next = second;
+
+    second->data = 20;
+    second->next = NULL;
+
+    head = first;
+
+    printf("Linked List:\n");
+
+    while(head != NULL)
+    {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+
+    printf("NULL");
+
+    return 0;
+}
