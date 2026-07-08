3c7421d19369f09f3d25eb50455dbf1e1bc094fe
5|-|4|-|1d <shahidsaleemitoo@gmail.com>
2026-07-08 12:15:03 +0530

    Implement Binary Search Tree in C

---
+++ Trees/Binary-Search-Tree.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node* createNode(int value)
+{
+    struct Node *newNode;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+void inorder(struct Node *root)
+{
+    if(root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+    struct Node *root;
+
+    root = createNode(20);
+    root->left = createNode(10);
+    root->right = createNode(30);
+
+    printf("BST Inorder Traversal:\n");
+
+    inorder(root);
+
+    return 0;
+}
