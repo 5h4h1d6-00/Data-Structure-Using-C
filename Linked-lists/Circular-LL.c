99ffe4439d94957f7aeedfad1b7a5de4f8a0e77f
5|-|4|-|1d <shahidsaleemitoo@gmail.com>
2026-07-08 12:11:44 +0530

    Implement circular linked list in Circular-LL.c

---
+++ Linked-lists/Circular-LL.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *first, *second, *third;
+
+    first = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+
+    first->data = 10;
+    first->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = first;
+
+    struct Node *ptr = first;
+
+    printf("Circular Linked List:\n");
+
+    do
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    while(ptr != first);
+
+    return 0;
+}
